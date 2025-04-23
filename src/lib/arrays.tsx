4b12fe041ff41f8d9542093d76ec18dea4b54ef7
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-04-23 16:11:44 +0200

    Rename util

--- src/lib/arrays.tsx
+++ src/lib/arrays.tsx
@@ -2,16 +2,16 @@ export type SortBy = "ALPHA_DESC" | "ALPHA_ASC";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getSortedObjects<T extends Record<string, any>>(
-  array: T[],
+  objects: T[],
   { key, sortBy }: { key: string; sortBy: SortBy }
 ) {
-  if (array.length && !Object.hasOwn(array[0], key)) {
+  if (objects.length && !Object.hasOwn(objects[0], key)) {
     throw new Error(
       `Cannot sort array of elements that do not have attribute "${key}".`
     );
   }
 
   return sortBy === "ALPHA_ASC"
-    ? [...array].sort((e1: T, e2: T) => (e1[key] < e2[key] ? -1 : 1))
-    : [...array].sort((e1: T, e2: T) => (e1[key] > e2[key] ? -1 : 1));
+    ? [...objects].sort((e1: T, e2: T) => (e1[key] < e2[key] ? -1 : 1))
+    : [...objects].sort((e1: T, e2: T) => (e1[key] > e2[key] ? -1 : 1));
 }
