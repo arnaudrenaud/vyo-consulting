99f41a636e0486420ca4196b16f0014776e1e90a
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-05-05 20:52:43 +0200

    Add theme variable, use it in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <nav className="fixed top-0 left-0 right-0 p-2 text-sm text-black bg-white">
+    <nav className="fixed top-0 left-0 right-0 p-2 text-sm text-black bg-mint-500">
       <Link href={PATHS.HOME}>Home</Link>
     </nav>
   );
