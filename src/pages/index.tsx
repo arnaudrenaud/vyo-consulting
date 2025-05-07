1328f738217b735cdc0a81e4e1aede9aad1b4e28
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-05-07 14:45:22 +0200

    Render expertises in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
+import { PortableText } from "@portabletext/react";
+
+import { client } from "@/sanity/lib/client";
+import { ALL_EXPERTISES_QUERY } from "@/sanity/lib/queries";
+import { ALL_EXPERTISES_QUERYResult } from "@/sanity/types";
+
 export async function getStaticProps() {
+  const expertises = await client.fetch(ALL_EXPERTISES_QUERY);
   return {
-    props: {},
+    props: { expertises },
   };
 }
 
-export default function Home() {
-  return "Index page";
+export default function Home({
+  expertises,
+}: {
+  expertises: ALL_EXPERTISES_QUERYResult;
+}) {
+  return (
+    <>
+      <h2>Nos expertises au service de votre transformation digitale.</h2>
+      <ul className="space-y-4">
+        {expertises.map((expertise) => (
+          <li key={expertise._id}>
+            <div>{expertise.name}</div>
+            {expertise.shortDescription && (
+              <PortableText value={expertise.shortDescription} />
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
