d10a73069da9ddb6ced796ddd889f9d5403c4f50
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-06-16 15:48:58 +0200

    Use either `SANITY_STUDIO_`-prefixed variable (Sanity CLI) or `NEXT_PUBLIC_` (Next build)

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,18 +1,20 @@
 export const apiVersion = "2025-04-14";
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
-  "Missing environment variable: SANITY_STUDIO_DATASET",
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET ||
+    process.env.SANITY_STUDIO_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_DATASET",
 );
 
 export const hostId = assertValue(
-  process.env.SANITY_STUDIO_HOST,
-  "Missing environment variable: SANITY_STUDIO_HOST",
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_HOST || process.env.SANITY_STUDIO_HOST,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_HOST",
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
-  "Missing environment variable: SANITY_STUDIO_PROJECT_ID",
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID ||
+    process.env.SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID",
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
