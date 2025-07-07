a6fbf636b28848e9840961bcac6f4c02cdddfd09
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-07 12:37:28 +0200

    Show solutions in order

--- src/components/SolutionsSection.tsx
+++ src/components/SolutionsSection.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import CardSolution from "./CardSolution";
 import ButtonLink from "./ButtonLink";
 import { ALL_EXPERTISES_QUERYResult } from "@/sanity/types";
+import { getSolutionsInOrder } from "@/helpers/functions";
 
 const SolutionsSection = ({
   expertises,
@@ -33,7 +34,7 @@ const SolutionsSection = ({
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8 mt-12">
-        {expertises.map((expertise) => (
+        {getSolutionsInOrder(expertises).map((expertise) => (
           <CardSolution key={expertise._id} {...expertise} />
         ))}
       </div>
