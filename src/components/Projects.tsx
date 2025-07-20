5978e804f9a3f7e5299d4bd766531fd7b3e174f8
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-20 23:02:44 +0200

    Add link to project card title

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,15 +2,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import Link from "next/link";
 import ButtonLink from "./ButtonLink";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ALL_PROJECTS_QUERYResult } from "@/sanity/types";
-import { urlFor } from "@/sanity/lib/image";
-import { RichContent } from "@/components/utils/RichContent";
-import { PATHS, SEARCH_PARAMS } from "@/helpers/constants";
-import { getSolutionThemeColor } from "@/helpers/functions";
+import { ProjectCard } from "@/components/projects/ProjectCard";
 
 const Projects = ({ projects }: { projects: ALL_PROJECTS_QUERYResult }) => {
   const [mounted, setMounted] = useState(false);
@@ -71,49 +67,7 @@ const Projects = ({ projects }: { projects: ALL_PROJECTS_QUERYResult }) => {
         <div className="w-full max-w-screen-lg px-4">
           <Slider {...settings}>
             {projects.slice(0, 6).map((project) => (
-              <div
-                key={project._id}
-                className={`bg-white rounded-2xl border-1 border-accent shadow-[3px_3px] ${getSolutionThemeColor(project.expertises[0].slug).background} w-[95%]! ml-[10px]! px-4 py-8 max-md:px-8`}
-              >
-                <div className="relative">
-                  <img
-                    src="/icones/projets-1.png"
-                    alt={`projet ${project.shortTitle}`}
-                  />
-                  {/* <img src={`/icones/projets-${n}.png`} alt={`projet ${n}`} /> */}
-                  <p className="absolute top-2.5 left-2.5 text-2xl shadow-[2px_2px_4px_rgba(0,0,0,0.25)] bg-white px-4 rounded-2xl py-1">
-                    {project.expertises.map((expertise) => (
-                      <img
-                        key={expertise._id}
-                        src={urlFor(expertise.logo).url()}
-                        alt={`Logo vyo.${expertise.name}`}
-                      />
-                    ))}
-                  </p>
-                </div>
-                <div className="flex items-center mt-6 mb-3">
-                  <img
-                    className="h-6 mr-[0.33rem]"
-                    src="/icones/folder-open.png"
-                    alt="folder open"
-                  />
-                  <h4 className="font-semibold">{project.shortTitle}</h4>
-                </div>
-                <p className="text-sm">
-                  <RichContent value={project.shortDescription} />
-                </p>
-                <Link
-                  href={`${PATHS.PROJECTS}?${SEARCH_PARAMS.PROJECT_ID}=${project._id}`}
-                  className="flex items-center text-xs font-medium mt-4"
-                >
-                  Voir plus
-                  <img
-                    src="/icones/ArrowRight.png"
-                    alt="ArrowRight"
-                    className="w-[16px] ml-2"
-                  />
-                </Link>
-              </div>
+              <ProjectCard project={project} key={project._id} />
             ))}
           </Slider>
         </div>
---
+++ src/components/projects/ProjectCard.tsx
@@ -0,0 +1,60 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { RichContent } from "@/components/utils/RichContent";
+import { PATHS, SEARCH_PARAMS } from "@/helpers/constants";
+import { getSolutionThemeColor } from "@/helpers/functions";
+import { urlFor } from "@/sanity/lib/image";
+import { ALL_PROJECTS_QUERYResult } from "@/sanity/types";
+import Link from "next/link";
+
+export function ProjectCard({
+  project,
+}: {
+  project: ALL_PROJECTS_QUERYResult[0];
+}) {
+  const projectUrl = `${PATHS.PROJECTS}?${SEARCH_PARAMS.PROJECT_ID}=${project._id}`;
+
+  return (
+    <div
+      className={`bg-white border-1 border-accent rounded-2xl shadow-[3px_3px] ${getSolutionThemeColor(project.expertises[0].slug).background} w-[95%]! ml-[10px]! px-4 py-8 max-md:px-8`}
+    >
+      <div className="relative">
+        <img src="/icones/projets-1.png" alt={`projet ${project.shortTitle}`} />
+        <p className="absolute top-2.5 left-2.5 text-2xl shadow-[2px_2px_4px_rgba(0,0,0,0.25)] bg-white pl-4 rounded-2xl py-1">
+          {project.expertises.map((expertise) => (
+            <img
+              key={expertise._id}
+              src={urlFor(expertise.logo).url()}
+              alt={`Logo vyo.${expertise.name}`}
+              className="h-5"
+            />
+          ))}
+        </p>
+      </div>
+      <div className="flex items-center mt-6 mb-3">
+        <img
+          className="h-6 mr-[0.33rem]"
+          src="/icones/folder-open.png"
+          alt="folder open"
+        />
+        <h4 className="font-semibold">
+          <Link href={projectUrl}>{project.shortTitle}</Link>
+        </h4>
+      </div>
+      <p className="text-sm">
+        <RichContent value={project.shortDescription} />
+      </p>
+      <Link
+        href={projectUrl}
+        className="flex items-center text-xs font-medium mt-4"
+      >
+        {project.shortTitle}
+        <img
+          src="/icones/ArrowRight.png"
+          alt="ArrowRight"
+          className="w-[16px] ml-2"
+        />
+      </Link>
+    </div>
+  );
+}
