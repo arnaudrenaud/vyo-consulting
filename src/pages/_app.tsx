d5d3715c473a617d1525994922ef4fe91d393d67
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-21 10:23:32 +0200

    Set `og:image` globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { PageLayout } from "@/components/PageLayout";
 import { PageLayoutData } from "@/helpers/getPageLayoutData";
+import { urlFor } from "@/sanity/lib/image";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -12,6 +13,10 @@ export default function App({
     throw new Error("Metadata is undefined.");
   }
 
+  const logo = pageProps.metadata.logo
+    ? urlFor(pageProps.metadata.logo).url()
+    : null;
+
   return (
     <>
       <Head>
@@ -20,6 +25,14 @@ export default function App({
           name="description"
           content={pageProps.metadata.description || ""}
           key="title"
+        />
+
+        {logo && <meta property="og:image" content={logo} />}
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="https://example.com/images/thumbnail.jpg"
         />
       </Head>
 
