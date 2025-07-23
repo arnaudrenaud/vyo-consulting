535408a8947b723803a38c0d2ef90af57b76cc71
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-23 08:15:33 +0200

    Hide form once sucessfully completed

--- src/components/about/Contact.tsx
+++ src/components/about/Contact.tsx
@@ -83,6 +83,7 @@ export function Contact() {
     form.trigger(); // revalidates the entire form
   }, [watchedValue, form]);
 
+  const [isFormShown, setIsFormShown] = useState(true);
   const [isPending, setIsPending] = useState(false);
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -90,12 +91,17 @@ export function Contact() {
     try {
       // Simule une requête async
       await new Promise((resolve) => setTimeout(resolve, 1000));
+      // await new Promise((resolve, reject) => setTimeout(reject, 1000));
+
       console.log("values ::: ", values);
       toast.success("Message envoyé avec succès !");
       form.reset(); // Réinitialise le formulaire
+      setIsFormShown(false);
     } catch (error) {
       console.log("error ::: ", error);
-      toast.error("Erreur lors de l’envoi. Réessayez.");
+      toast.error(
+        "Erreur lors de l’envoi. Veuillez réessayer ou vérifier votre connexion à Internet.",
+      );
     } finally {
       setIsPending(false);
     }
@@ -119,219 +125,229 @@ export function Contact() {
       </div>
       <div className="h-[1px] w-full bg-[#c9cdd2]"></div>
 
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <FormField
-            control={form.control}
-            name="objectif"
-            render={({ field }) => (
-              <FormItem className="flex flex-col space-y-3 mt-4">
-                <div className="flex space-x-2">
-                  <FormControl>
-                    <Checkbox
-                      id="objectif-solution"
-                      checked={field.value === "solution"}
-                      onCheckedChange={() => field.onChange("solution")}
-                      className="h-5 w-5 data-[state=checked]:bg-black data-[state=checked]:border-black hover:cursor-pointer"
-                    />
-                  </FormControl>
-                  <FormLabel
-                    htmlFor="objectif-solution"
-                    className="text-sm text-[#0A0A0A]"
-                  >
-                    Je recherche une solution adaptée à mon besoin
-                  </FormLabel>
-                </div>
-                <div className="flex space-x-2">
-                  <FormControl>
-                    <Checkbox
-                      id="objectif-postule"
-                      checked={field.value === "postule"}
-                      onCheckedChange={() => field.onChange("postule")}
-                      className="h-5 w-5 data-[state=checked]:bg-black data-[state=checked]:border-black hover:cursor-pointer"
-                    />
-                  </FormControl>
-                  <FormLabel
-                    htmlFor="objectif-postule"
-                    className="text-sm text-[#0A0A0A]"
-                  >
-                    Je postule
-                  </FormLabel>
-                </div>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <div className="h-[1px] w-full bg-[#c9cdd2]"></div>
-
-          <div className="grid grid-cols-3 max-lg:grid-cols-1 gap-6">
-            <div>
-              {/* Prénom */}
-              <FormField
-                control={form.control}
-                name="prenom"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="max-lg:text-xs">
-                      <span>
-                        Prénom <span className="text-destructive">*</span>
-                      </span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Votre prénom"
-                        {...field}
-                        className="text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Nom */}
-              <FormField
-                control={form.control}
-                name="nom"
-                render={({ field }) => (
-                  <FormItem className="mt-6">
-                    <FormLabel className="max-lg:text-xs">
-                      <span>
-                        Nom <span className="text-destructive">*</span>
-                      </span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Votre nom"
-                        {...field}
-                        className="text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Email */}
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="mt-6">
-                    <FormLabel className="max-lg:text-xs">
-                      <span>
-                        Email <span className="text-destructive">*</span>
-                      </span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="Votre email"
-                        {...field}
-                        className="text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div>
-              {/* Entreprise */}
-
-              <FormField
-                control={form.control}
-                name="entreprise"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="max-lg:text-xs">
-                      <span>
-                        Entreprise
-                        {form.getValues("objectif") === "solution" ? (
-                          <>
-                            {" "}
-                            <span className="text-destructive">*</span>
-                          </>
-                        ) : (
-                          ""
-                        )}
-                      </span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Votre entreprise"
-                        {...field}
-                        className="text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Poste */}
-              <FormField
-                control={form.control}
-                name="poste"
-                render={({ field }) => (
-                  <FormItem className="mt-6">
-                    <FormLabel className="max-lg:text-xs">
-                      <span>
-                        Intitulé de votre poste
-                        {form.getValues("objectif") === "solution" ? (
-                          <>
-                            {" "}
-                            <span className="text-destructive">*</span>
-                          </>
-                        ) : (
-                          ""
-                        )}
-                      </span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder="Votre poste"
-                        {...field}
-                        className="text-sm"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            {/* Message */}
+      {isFormShown ? (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
             <FormField
               control={form.control}
-              name="message"
+              name="objectif"
               render={({ field }) => (
-                <FormItem className="!block">
-                  <FormLabel className="mb-2 max-lg:text-xs">Message</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Votre message"
-                      className="h-4/5 max-md:h-[200px] max-lg:h-[250px] mb-[7px] text-sm"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>500 caractères max.</FormDescription>
+                <FormItem className="flex flex-col space-y-3 mt-4">
+                  <div className="flex space-x-2">
+                    <FormControl>
+                      <Checkbox
+                        id="objectif-solution"
+                        checked={field.value === "solution"}
+                        onCheckedChange={() => field.onChange("solution")}
+                        className="h-5 w-5 data-[state=checked]:bg-black data-[state=checked]:border-black hover:cursor-pointer"
+                      />
+                    </FormControl>
+                    <FormLabel
+                      htmlFor="objectif-solution"
+                      className="text-sm text-[#0A0A0A]"
+                    >
+                      Je recherche une solution adaptée à mon besoin
+                    </FormLabel>
+                  </div>
+                  <div className="flex space-x-2">
+                    <FormControl>
+                      <Checkbox
+                        id="objectif-postule"
+                        checked={field.value === "postule"}
+                        onCheckedChange={() => field.onChange("postule")}
+                        className="h-5 w-5 data-[state=checked]:bg-black data-[state=checked]:border-black hover:cursor-pointer"
+                      />
+                    </FormControl>
+                    <FormLabel
+                      htmlFor="objectif-postule"
+                      className="text-sm text-[#0A0A0A]"
+                    >
+                      Je postule
+                    </FormLabel>
+                  </div>
                   <FormMessage />
                 </FormItem>
               )}
             />
-          </div>
 
-          <Button type="submit" className="block ml-auto" disabled={isPending}>
-            <span className="space-x-2">
-              {isPending ? (
-                <Loader2Icon className="inline animate-spin" />
-              ) : null}
-              <span>Envoyer</span>
-            </span>
-          </Button>
-        </form>
-      </Form>
+            <div className="h-[1px] w-full bg-[#c9cdd2]"></div>
+
+            <div className="grid grid-cols-3 max-lg:grid-cols-1 gap-6">
+              <div>
+                {/* Prénom */}
+                <FormField
+                  control={form.control}
+                  name="prenom"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="max-lg:text-xs">
+                        <span>
+                          Prénom <span className="text-destructive">*</span>
+                        </span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Votre prénom"
+                          {...field}
+                          className="text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Nom */}
+                <FormField
+                  control={form.control}
+                  name="nom"
+                  render={({ field }) => (
+                    <FormItem className="mt-6">
+                      <FormLabel className="max-lg:text-xs">
+                        <span>
+                          Nom <span className="text-destructive">*</span>
+                        </span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Votre nom"
+                          {...field}
+                          className="text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Email */}
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="mt-6">
+                      <FormLabel className="max-lg:text-xs">
+                        <span>
+                          Email <span className="text-destructive">*</span>
+                        </span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="Votre email"
+                          {...field}
+                          className="text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div>
+                {/* Entreprise */}
+
+                <FormField
+                  control={form.control}
+                  name="entreprise"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="max-lg:text-xs">
+                        <span>
+                          Entreprise
+                          {form.getValues("objectif") === "solution" ? (
+                            <>
+                              {" "}
+                              <span className="text-destructive">*</span>
+                            </>
+                          ) : (
+                            ""
+                          )}
+                        </span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Votre entreprise"
+                          {...field}
+                          className="text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                {/* Poste */}
+                <FormField
+                  control={form.control}
+                  name="poste"
+                  render={({ field }) => (
+                    <FormItem className="mt-6">
+                      <FormLabel className="max-lg:text-xs">
+                        <span>
+                          Intitulé de votre poste
+                          {form.getValues("objectif") === "solution" ? (
+                            <>
+                              {" "}
+                              <span className="text-destructive">*</span>
+                            </>
+                          ) : (
+                            ""
+                          )}
+                        </span>
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="Votre poste"
+                          {...field}
+                          className="text-sm"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              {/* Message */}
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem className="!block">
+                    <FormLabel className="mb-2 max-lg:text-xs">
+                      Message
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        placeholder="Votre message"
+                        className="h-4/5 max-md:h-[200px] max-lg:h-[250px] mb-[7px] text-sm"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>500 caractères max.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <Button
+              type="submit"
+              className="block ml-auto"
+              disabled={isPending}
+            >
+              <span className="space-x-2">
+                {isPending ? (
+                  <Loader2Icon className="inline animate-spin" />
+                ) : null}
+                <span>Envoyer</span>
+              </span>
+            </Button>
+          </form>
+        </Form>
+      ) : (
+        <p className="p-8 text-center">Message envoyé avec succès !</p>
+      )}
     </section>
   );
 }
