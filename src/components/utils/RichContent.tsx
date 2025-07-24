bc71ca563bc62fbf238d58217bb54bea2860c770
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-24 17:31:22 +0200

    Render headings from CMS content in large, bold font

--- src/components/utils/RichContent.tsx
+++ src/components/utils/RichContent.tsx
@@ -7,6 +7,18 @@ export function RichContent(props: Parameters<typeof PortableText>[0]) {
       components={{
         block: {
           normal: ({ children }) => <>{children}</>,
+          h1: ({ children }) => (
+            <h1 className="text-3xl text-bold">{children}</h1>
+          ),
+          h2: ({ children }) => (
+            <h1 className="text-2xl text-bold">{children}</h1>
+          ),
+          h3: ({ children }) => (
+            <h1 className="text-xl text-bold">{children}</h1>
+          ),
+          h4: ({ children }) => (
+            <h1 className="text-lg text-bold">{children}</h1>
+          ),
         },
         ...props.components,
       }}
