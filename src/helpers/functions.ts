e3a1ded5f094fe15c40dcaf8642fed33c6a6c1b6
Arnaud Renaud <arnaud.renaud@gmail.com>
2026-07-17 12:15:26 +0200

    Allow a solution to be removed in CMS

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -5,14 +5,15 @@ import { Slug } from "@/sanity/types";
 export function getSolutionsInOrder<T extends { slug: Slug }>(
   solutions: T[],
 ): T[] {
-  return SOLUTIONS_IN_ORDER.map((_slug) => {
-    const solution = solutions.find(({ slug }) => slug.current === _slug);
-    if (!solution) {
-      throw new Error(
-        `Solution (or expertise) ${_slug} not found – maybe it was not added in CMS.`,
-      );
-    }
-    return solution;
+  return [...solutions].sort((a, b) => {
+    const orderA = SOLUTIONS_IN_ORDER.indexOf(a.slug.current);
+    const orderB = SOLUTIONS_IN_ORDER.indexOf(b.slug.current);
+
+    if (orderA === -1 && orderB === -1) return 0;
+    if (orderA === -1) return 1;
+    if (orderB === -1) return -1;
+
+    return orderA - orderB;
   });
 }
 export function getSolutionThemeColor(slug: Slug) {
