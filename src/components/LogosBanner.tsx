a905dca406b2191cc8ae32075cad96af657069e6
Yaghmurassen <sainsony@gmail.com>
2026-07-31 16:31:24 +0200

    title format

--- src/components/LogosBanner.tsx
+++ src/components/LogosBanner.tsx
@@ -23,7 +23,15 @@ export default function LogosBanner({
         className={`${styles.section} ${GeistSans.className}`}
         aria-label={title}
       >
-        <p className={styles.title}>{title}</p>
+        <div className="text-center mb-16">
+          <div className="inline-block relative">
+            <h2 className="font-light text-5xl max-md:text-3xl">{title}</h2>
+            <div className="absolute left-0 -bottom-[8px] w-full flex items-center justify-between h-[1px]">
+              <div className="bg-black h-[2px] w-[calc(100%-8px)]"></div>
+              <div className="w-1 h-1 bg-black rounded-full ml-1"></div>
+            </div>
+          </div>
+        </div>
 
         <div className={styles.fixedLayout}>
           <ul className={styles.grid}>
